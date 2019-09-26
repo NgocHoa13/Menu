862ea332c9e7c4e061198e93eb5603ded81057b7
ngochoa <nguyenngochoa3979@gmail.com>
2019-09-26 20:58:52 +0700

    first commit

---
+++ app/src/main/java/nguyenngochoa3979/com/menu/MainActivity.java
@@ -0,0 +1,23 @@
+package nguyenngochoa3979.com.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mennu_demo, menu);
+
+        return true;
+    }
+}
